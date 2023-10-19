29523a33dd3ceaea25feebdbef2f74bea70bb0a3
danecluster <danecluster@gmail.com>
2023-10-19 18:31:42 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
+const path= require('path')
+const fs=require('fs')
+const generateMarkdown=require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -16,11 +19,34 @@ const questions = [
         type: 'input',
         name: 'installation',
         message: 'How do you install your project?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Leave your email if you like to contribute.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is this app used?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Are there any testing frameworks?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Does this app contain a Github license?',
+        choices: ['MIT','BSD 3', 'GPL 3.0', 'none']
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(fileName),data)
+}
 
 // TODO: Create a function to initialize app
 function init() {
@@ -28,13 +54,11 @@ function init() {
   .prompt(questions)
   .then((answers) => {
     // Use user feedback for... whatever!!
+    console.log({...answers})
+    writeToFile('readMe.md',generateMarkdown({...answers}))
   })
   .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+    console.error(error)
   });
 }
 
